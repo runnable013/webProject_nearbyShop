0c782397195ced010fda82733d269ae30a5ab101
Runnable <42142686+runnable013@users.noreply.github.com>
2018-10-20 16:01:11 +0900

    가독성 수정

--- js/item.js
+++ js/item.js
@@ -15,17 +15,17 @@ var key_list = new Array(10),
 
 var kategorie = new Array("식품", "전자제품", "가구");
 
-function enterKey(){
-  if(event.keyCode == 13){
-    input();
-  }
-}
+function enterKey()
+{ if(event.keyCode == 13){ input(); } }
 
 //입력된 정보를 저장
-function input() {
-  if(point < 10){
+function input()
+{
+  if(point < 10)
+  {
     name_list[point] = document.getElementById("name").value;
-    if (name_list[point] != "") {
+    if (name_list[point] != "")
+    {
       key_list[point] = superKey;
       item1_list[point] = document.getElementById("kategorie1").value;
       item2_list[point] = document.getElementById("kategorie2").value;
@@ -38,18 +38,17 @@ function input() {
       superKey++
     }
   }
-  else{
-    alert("페이지기능 추가예정");
-  }
+  else
+  {  alert("페이지기능 추가예정"); }
 }
 
 //저장된 카테고리 출력
-window.onload = function() {
-  for (i = 0; i < kategorie.length; i++) {
-    document.getElementById("kategorie1").options[i] = new Option(
-      kategorie[i],
-      i
-    );
+window.onload = function()
+{
+  for (i = 0; i < kategorie.length; i++)
+  {
+    document.getElementById("kategorie1").options[i] =
+    new Option( kategorie[i], i);
   }
 };
 
@@ -63,9 +62,10 @@ function itemChange() {
 
 // 저장된 정보를 출력
 // 1.페이지 기능추가, 2.나중에 'input동적 생성부분' 수정
-var add_item = new Array();
-function output() {
-  add_item[point] =
+var outputItem = new Array();
+function output()
+{
+  outputItem[point] =
     '<div id="items' +
     superKey +
     '"> <div class="item" style="width:20px"> &nbsp </div> <div class="item"> 제품명: ' +
@@ -79,14 +79,17 @@ function output() {
     '" onclick="removeItem(' +
     key_list[point] +
     ')">X</button> </div>';
-  test2.innerHTML = add_item.join("");
+  output.innerHTML = outputItem.join("");
 }
 
 //정보 삭제
-function removeItem(remKey) {
-  for(i=0; i<key_list.length; i++){
-    if(key_list[i]==remKey){
-      add_item.splice(i, 1);
+function removeItem(remKey)
+{
+  for(i=0; i<key_list.length; i++)
+  {
+    if(key_list[i]==remKey)
+    {
+      outputItem.splice(i, 1);
       key_list.splice(i, 1);
       name_list.splice(i, 1);
       item1_list.splice(i, 1);
@@ -94,7 +97,9 @@ function removeItem(remKey) {
       break;
     }
   }
+
   var delItem = document.getElementById("items"+remKey);
   delItem.remove();
   point--;
 }
+
